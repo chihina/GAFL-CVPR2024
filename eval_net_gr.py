0071090782c6b9ff461d6959ab171282b8ed15b2
chihina <program.student.tti@gmail.com>
2024-12-23 09:14:16 +0900

    update envs

--- eval_net_gr.py
+++ eval_net_gr.py
@@ -20,8 +20,6 @@ from sklearn.manifold import TSNE
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans
-from fast_pytorch_kmeans import KMeans as KMeansGPU
-# from torchmetrics.clustering import AdjustedRandScore, NormalizedMutualInfoScore
 
 import warnings 
 warnings.filterwarnings("ignore")
@@ -30,7 +28,6 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from munkres import Munkres
 
 from config import *
 from volleyball import *
@@ -234,20 +231,6 @@ def evaluate_gar_metrics(mode, info_train, info_test, metrics_dic, cfg):
     feat_dic = {'people':'individual_features', 'scene':'group_features'}
     match_score = torch.cdist(info_test[feat_dic[mode]], info_train[feat_dic[mode]], p=2)
     match_idx = torch.argmin(match_score, dim=-1)
-
-    # print('===> Calculate clustering accuracy')
-    # ga_gt_labels = info_test['activities_in_all'].long()
-    # for num_cluster in [8, 16, 32]:
-    #     kmeans_gpu = KMeansGPU(n_clusters=num_cluster, mode='euclidean', verbose=0)
-    #     ga_cluster_labels = kmeans_gpu.fit_predict(info_test[feat_dic[mode]]).long()
-    #     nmi_score = NormalizedMutualInfoScore("arithmetic")
-    #     ga_nmi_score = nmi_score(ga_cluster_labels, ga_gt_labels).to('cpu').detach().item()
-    #     print(f'NRI ({mode}) k={num_cluster}', ga_nmi_score)
-    #     metrics_dic[f'NRI ({mode}) k={num_cluster}'] = ga_nmi_score
-    #     ari_score = AdjustedRandScore()
-    #     ga_ari_score = ari_score(ga_cluster_labels, ga_gt_labels).to('cpu').detach().item()
-    #     print(f'ARI ({mode}) k={num_cluster}', ga_ari_score)
-    #     metrics_dic[f'ARI ({mode}) k={num_cluster}'] = ga_ari_score
 
     print("===> Save a nearest neighbor video id")
     save_nn_video(mode, cfg, info_test, info_train, match_idx)
